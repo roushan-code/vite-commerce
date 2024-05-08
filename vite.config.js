a0115c36ac827760d4ec3af33546ad2e3feb570d
Roushan <roushansingh3698@gmail.com>
2024-05-08 15:38:25 +0530

    try

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://ecommerce-server-side-sigma.vercel.app',
+        target: 'https://ecommerce-server-side-sigma.vercel.app/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
